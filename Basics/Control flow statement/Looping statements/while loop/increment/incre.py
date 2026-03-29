2effb471b757b1b4fb56fbd46c0d11cbfa655eef
Chaluvaraj <chaluvashetty23@gmail.com>
2026-03-29 23:30:07 +0530

    updated

---
+++ Basics/Control flow statement/Looping statements/while loop/increment/incre.py
@@ -0,0 +1,42 @@
+# i = 1
+
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# print("==============")
+
+# i = 2
+
+# while i <= 10:
+#     print(i)
+#     i += 2
+
+# print("==============")
+
+# i = 1
+# total = 0
+
+# while i <= 5:
+#     total += i
+#     i += 1
+
+# print("Sum:", total)
+
+# print("==============")
+
+# password = ""
+
+# while password != "1234":
+#     password = input("Enter password: ")
+
+# print("Access granted")
+
+# print("==============")
+
+i = 1
+num = 3
+
+while i <= 10:
+    print(num, "x", i, "=", num * i)
+    i += 1
