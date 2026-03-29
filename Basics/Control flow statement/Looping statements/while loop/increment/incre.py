a16a0fc36a5cf39c350a339be395a246b1b01440
Chaluvaraj <chaluvashetty23@gmail.com>
2026-03-29 23:30:46 +0530

    updated

--- Basics/Control flow statement/Looping statements/while loop/increment/incre.py
+++ Basics/Control flow statement/Looping statements/while loop/increment/incre.py
@@ -1,42 +1,40 @@
-# i = 1
+#Example 1: Print 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
 
-# while i <= 5:
-#     print(i)
-#     i += 1
+print("==============")
 
-# print("==============")
+# Example 2: Print Even Numbers
+i = 2
+while i <= 10:
+    print(i)
+    i += 2
 
-# i = 2
+print("==============")
 
-# while i <= 10:
-#     print(i)
-#     i += 2
+# Example 3: Calculate the sum of first 5 natural numbers
+i = 1
+total = 0
+while i <= 5:
+    total += i
+    i += 1
+print("Sum:", total)
 
-# print("==============")
+print("==============")
 
-# i = 1
-# total = 0
+# Example 4: Password Validation
+password = ""
+while password != "1234":
+    password = input("Enter password: ")
+print("Access granted")
 
-# while i <= 5:
-#     total += i
-#     i += 1
+print("==============")
 
-# print("Sum:", total)
-
-# print("==============")
-
-# password = ""
-
-# while password != "1234":
-#     password = input("Enter password: ")
-
-# print("Access granted")
-
-# print("==============")
-
+# Example 5: Multiplication Table of 3
 i = 1
 num = 3
-
 while i <= 10:
     print(num, "x", i, "=", num * i)
     i += 1
