97e9fc616dac61fe1a48f201fddb57c99f658547
Chaluvaraj <chaluvashetty23@gmail.com>
2026-03-31 10:41:44 +0530

    updated

---
+++ Arrays and List/Bubble sort/prog1.py
@@ -0,0 +1,18 @@
+def bubblesort(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+def createarray():
+    print("enter the element into the arr to be created")
+    l1=[]
+    while True:
+        try:
+            n=int(input("enter the value:"))
+            l1.append(n)
+        except Exception as e:
+            return l1
+arr=createarray()
+res = bubblesort(arr)
+print("Sorted array is:", res)
