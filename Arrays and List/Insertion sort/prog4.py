374447733aef97947ed5f8770bef300bad469398
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-02 09:54:44 +0530

    updated

---
+++ Arrays and List/Insertion sort/prog4.py
@@ -0,0 +1,31 @@
+#right rotation 
+
+def reversalarray(arr,i,j):
+    while i < j:
+        arr[i],arr[j]=arr[j],arr[i]
+        i += 1
+        j -= 1
+def rightrotation(arr,k):
+    n = len(arr)
+    if k >= n:
+        k = k%n
+    reversalarray(arr,0,(n-1))
+    reversalarray(arr,0,(k-1))
+    reversalarray(arr, k, (n-1))
+   
+
+def createarray():
+    print("enter the element into the arr to be created")
+    l1=[]
+    while True:
+        try:
+            n=int(input("enter the value:"))
+            l1.append(n)
+        except Exception as e:
+            return l1
+        
+arr = createarray()
+k=int(input("Enter a num:"))
+print("original array:", arr)
+rightrotation(arr,k)
+print("rightrotation:", arr)
