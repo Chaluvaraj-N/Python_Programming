74210aea4b1b6a04433394aa6203e642382eda6f
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-05 23:40:17 +0530

    updated

---
+++ Numericals/Arm strong numbers/prog5.py
@@ -0,0 +1,44 @@
+#wap to print ASN and Non ASN values separtely present in a user defined range
+
+def countdigits(n):
+    count = 0
+    while n > 0:
+        n = n // 10
+        count = count + 1
+    return count   
+
+def isASN(num):
+    temp = num
+
+    if num < 0:
+        num = num * -1
+
+    pow = countdigits(num)   
+    asn = 0                 
+
+    while num > 0:
+        base = num % 10      
+        asn = asn + (base ** pow)
+        num = num // 10      
+
+    if temp < 0:
+        asn = asn * -1
+
+    return asn == temp
+
+
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the last number: "))
+
+if start > end:
+    print("invalid input")
+else:
+    print("ASN values: ")
+    for i in range(start, end + 1):
+        if isASN(i):
+            print(i, end=" ")
+
+    print("\n Non-ASN values: ")
+    for i in range(start, end + 1):
+        if not isASN(i):
+            print(i, end=" ")
