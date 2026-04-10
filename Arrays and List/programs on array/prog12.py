eba37250ca002a309df132cdd862d4135ad0abe9
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-10 10:11:06 +0530

    updated

--- Arrays and List/programs on array/prog12.py
+++ Arrays and List/programs on array/prog12.py
@@ -11,9 +11,9 @@ class Solution:
         
         #the peak is in b/w
         for i in range(1, n - 1):
-            
              if arr[i - 1] < arr[i] > arr[i + 1]:
                 return i
+             
         #if the peak is in the last index
         if arr[n - 1] > arr[n - 2]:
             return (n - 1)
