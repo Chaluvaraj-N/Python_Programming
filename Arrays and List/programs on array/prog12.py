5eb5cc5b978a2069f3315dcccc81f886e7b50c3d
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-10 10:02:25 +0530

    updated

---
+++ Arrays and List/programs on array/prog12.py
@@ -0,0 +1,17 @@
+#peak element
+
+class Solution:   
+    def peakElement(self, arr):
+        # Code here
+        n = len(arr)
+        #the length of the array is n==1
+        if n == 1:
+            return 0
+        #the peak is in b/w
+        for i in range(1, n - 1):
+            if arr[i - 1] < arr[i] > arr[i + 1]:
+                return i
+        #if the peak is in the last index
+        if arr[n - 1] > arr[n - 2]:
+            return (n - 1)
+        return 0
