2aadba349d73b625e6d6ae7bb0102452cecf737c
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-14 09:48:11 +0530

    updated

--- Strings/prog4.py
+++ Strings/prog4.py
@@ -6,12 +6,23 @@ def sumCharacterDigits(s):
     res = ""
     sumCharDig = 0
 
+    # separate digits and characters
     for i in range(len(s)):
         if "0" <= s[i] <= "9":
             sumCharDig = sumCharDig + (ord(s[i]) - 48)
         else:
             res = res + s[i]
 
+    # -------- LOGIC 1 (manual conversion: int → string) --------
+    # num = ""
+    # while sumCharDig > 0:
+    #     rem = sumCharDig % 10
+    #     num = chr(rem + 48) + num
+    #     sumCharDig = sumCharDig // 10
+    # return res + num
+
+
+    # -------- LOGIC 2 (easy method using str) --------
     num = str(sumCharDig)
     return res + num
 
