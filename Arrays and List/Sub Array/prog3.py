a6f4ed1cffef44a7a50492976f9c74779ccff789
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-22 08:59:28 +0530

    updated

---
+++ Arrays and List/Sub Array/prog3.py
@@ -0,0 +1,16 @@
+#maximum product subarray  (leetcode 152)
+
+class Solution:
+    def maxProduct(self, nums) -> int:
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = nums[0]
+
+        for i in range(1, len(nums)):
+            current = nums[i]
+            temp_max = max(current, max_so_far * current, min_so_far * current)
+            min_so_far = min(current, max_so_far * current, min_so_far * current)
+            max_so_far = temp_max
+            result = max(result, max_so_far)
+
+        return result
